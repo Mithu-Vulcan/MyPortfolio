2f013a24da6d313f3d3206b6dffe636d9313d78e
Mithu-Vulcan <mithushanthjonathanparrot@gmail.com>
2025-06-22 22:42:24 +0530

    added some gsap to test

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleMenu () {
     document.getElementById("section-nav").classList.toggle('active');
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger, SplitText)
+
+    let tl = gsap.timeline()
+
+    tl.from("#hero-stagger", {
+        y: 200,
+        duration: 0.6,
+        ease: "power1.inOut",
+        opacity: 0,
+        stagger: 0.2
+    })
+})
