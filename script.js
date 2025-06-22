b8ba970ad1916fb5ea13c7d08735be16769ff442
Mithu-Vulcan <mithushanthjonathanparrot@gmail.com>
2025-06-22 23:00:05 +0530

    added preload and some more gsap

--- script.js
+++ script.js
@@ -7,11 +7,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     let tl = gsap.timeline()
 
+    tl.from(".header-name", {
+        y: -200,
+        opacity: 0,
+        duration: 0.6,
+    })
+
+    tl.from('.nav-item', {
+        y: -200,
+        opacity: 0,
+        duration: 0.3,
+        stagger: 0.1,
+        ease: "elasic"
+    })
+
     tl.from("#hero-stagger", {
         y: 200,
         duration: 0.6,
         ease: "power1.inOut",
         opacity: 0,
         stagger: 0.2
+    })
+
+    tl.from("#hero-vector", {
+        opacity: 0,
+        duration: 0.6,
+        x: 400
+    })
+
+    tl.from(".contact-btn", {
+        opacity: 0,
+        duration: 0.6,
+        
     })
 })
